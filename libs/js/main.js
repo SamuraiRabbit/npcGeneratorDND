c521df48bb057de56e1a58f161d2c630076550d2
Christian Doyle <80451970+SamuraiRabbit@users.noreply.github.com>
2022-05-30 08:14:05 +0100

    Basic Structure
    Core HTML structure and click event set up.

--- libs/js/main.js
+++ libs/js/main.js
@@ -1,9 +1,37 @@
+/*-----GLOBAL VARIABLES-----*/
+const raceArray = ["Dwarf", "Elf", "Halfling", "Human", "Dragonborn", "Gnome", "Half-Elf", "Half-Orc", "Tiefling"];
+const genderArray = ["Male", "Female", "Non-Binary"];
+const occupationArray = ["Blacksmith", "Labourer", "Sailor"];
+const demeanourArray = ["Surly", "Mysterious", "Exhausted", "Happy"];
+
+
 /*-----ON LOAD-----*/
 $(window).on('load', function () {
     // Launches pre-loader while html loads
     if ($('#preloader').length) {
         $('#preloader').delay(1000).fadeOut('slow', function () {
-        $(this).remove();
         });
     }   
   });
+
+/*-----ON READY-----*/
+$(document).ready(function(){
+
+    $('#npcButton').click(function() {
+        generateRace(raceArray);
+        generateGender(genderArray);
+        generateOccupation(occupationArray);
+        generateDemeanour(demeanourArray);
+    });
+});
+
+function generateRace(raceArray) {
+    $race = Math.floor(Math.random() * raceArray.length);
+    $("#race").text(raceArray[$race]);
+    alert(raceArray[randomRace]);
+}
+
+
+
+
+
